ad04157d9de6fd32caf2a7347928b00e8e670332
W Riddle <riddle.w@gmail.com>
2022-06-01 21:38:34 -0400

    June 1, 2022

---
+++ 8kyu/playing_banjo/playing_banjo.test.ts
@@ -0,0 +1,15 @@
+// See https://www.chaijs.com for how to use Chai.
+import { assert } from "chai";
+
+import { areYouPlayingBanjo } from "./playing_banjo";
+
+// TODO Add your tests here
+describe("Are you playing banjo?", () => {
+  it("Fixed test", () => {
+    assert.strictEqual(areYouPlayingBanjo("Adam"), "Adam does not play banjo");
+    assert.strictEqual(areYouPlayingBanjo("Paul"), "Paul does not play banjo");
+    assert.strictEqual(areYouPlayingBanjo("Ringo"), "Ringo plays banjo");
+    assert.strictEqual(areYouPlayingBanjo("bravo"), "bravo does not play banjo");
+    assert.strictEqual(areYouPlayingBanjo("rolf"), "rolf plays banjo");
+  })
+});
